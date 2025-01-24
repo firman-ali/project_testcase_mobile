b732a04ba5ec01a2117ccdc21bc36fd28d8b7026
firman-ali <firmanali22@gmail.com>
2025-01-24 18:38:33 +0800

    add report approval

--- lib/app/modules/report_approval/controllers/report_approval_controller.dart
+++ lib/app/modules/report_approval/controllers/report_approval_controller.dart
@@ -1,23 +1,87 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+import '../../../models/report_response.dart';
+import '../../../repository/app_repository.dart';
+import '../../../repository/app_repository_impl.dart';
+import '../../../utils/error_response_helper.dart';
+import '../../../utils/utils.dart';
+import '../../widgets/rounded_dialog.dart';
 
 class ReportApprovalController extends GetxController {
-  //TODO: Implement ReportApprovalController
+  final PagingController<int, ReportRecord> pagingController =
+      PagingController(firstPageKey: 1);
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    getData(page: 1);
   }
 
   @override
   void onReady() {
     super.onReady();
+    pagingController.addPageRequestListener((pageKey) {
+      getData(page: pageKey);
+    });
   }
 
   @override
   void onClose() {
     super.onClose();
+    pagingController.dispose();
   }
 
-  void increment() => count.value++;
+  Future<void> onRefresh() async {
+    pagingController.refresh();
+  }
+
+  Future<void> getData({required int page}) async {
+    try {
+      AppRepository repo = AppRepositoryImpl();
+      final response = await repo.getReports(page: page);
+      final isLastPage = response.records.length < 10;
+      if (isLastPage) {
+        pagingController.appendLastPage(response.records);
+      } else {
+        pagingController.appendPage(response.records, page + 1);
+      }
+    } on ReturnErrorException catch (e) {
+      Get.snackbar(
+        "Error Mengambil Data",
+        "${e.msg}, Please try again",
+        snackPosition: SnackPosition.BOTTOM,
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
+        margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
+        maxWidth: Get.width,
+      );
+      pagingController.appendLastPage([]);
+    } catch (e) {
+      Get.snackbar(
+        "Error Mengambil Data",
+        "Please try again or contact system administrator",
+        snackPosition: SnackPosition.BOTTOM,
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
+        margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
+        maxWidth: Get.width,
+      );
+      pagingController.appendLastPage([]);
+    }
+  }
+
+  Future<bool> confirmData() async {
+    Get.dialog(
+      RoundedDialog(
+        color: Colors.blue,
+        title: "Terima Pengajuan Laporan",
+        content: "Apakah Anda yakin menerima laporan ini?",
+        negativeLabelButton: "Tidak",
+        positiveLabelButton: "Yakin dan Terima",
+        negativeOnPressed: Get.back,
+        positiveOnPressed: Utils.backPressed,
+      ),
+    );
+    return Future.value(false);
+  }
 }
--- lib/app/modules/report_approval/views/report_approval_view.dart
+++ lib/app/modules/report_approval/views/report_approval_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
+import '../../../models/report_response.dart';
+import '../../../utils/utils.dart';
+import '../../widgets/empty_data.dart';
 import '../controllers/report_approval_controller.dart';
 
 class ReportApprovalView extends GetView<ReportApprovalController> {
@@ -10,14 +14,209 @@ class ReportApprovalView extends GetView<ReportApprovalController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ReportApprovalView'),
-        centerTitle: true,
+        title: const Text("Approval Laporan"),
       ),
-      body: const Center(
-        child: Text(
-          'ReportApprovalView is working',
-          style: TextStyle(fontSize: 20),
+      body: PagedListView<int, ReportRecord>.separated(
+        padding: const EdgeInsets.all(24.0),
+        pagingController: controller.pagingController,
+        builderDelegate: PagedChildBuilderDelegate<ReportRecord>(
+          itemBuilder: (context, item, index) => ApprovalReportCard(
+            report: item,
+            onClick: controller.confirmData,
+          ),
+          firstPageProgressIndicatorBuilder: (_) =>
+              const ShimmerApprovalReportCard(),
+          newPageProgressIndicatorBuilder: (_) =>
+              const ShimmerApprovalReportCard(),
+          noItemsFoundIndicatorBuilder: (_) => const EmptyData(),
         ),
+        separatorBuilder: (_, index) => const SizedBox(height: 16.0),
+      ),
+    );
+  }
+}
+
+class ApprovalReportCard extends StatelessWidget {
+  const ApprovalReportCard({super.key, required this.report, this.onClick});
+
+  final ReportRecord report;
+  final void Function()? onClick;
+
+  Color getStatusColor(String status) {
+    switch (status) {
+      case "selesai":
+        return Colors.blue;
+      case "diproses":
+        return Colors.purple;
+      case "diterima":
+        return Colors.green;
+      case "ditolak":
+        return Colors.red;
+    }
+    return Colors.green;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: report.reportStatus == "diproses" ? onClick : null,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: Colors.grey),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+              decoration: BoxDecoration(
+                color: getStatusColor(report.reportStatus.toLowerCase()),
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(90),
+              ),
+              child: Text(
+                report.reportStatus.capitalizeFirst ?? "",
+                style: GoogleFonts.plusJakartaSans(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            Text(
+              report.reportTitle,
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            Row(
+              children: [
+                const Icon(
+                  Icons.calendar_month,
+                  size: 18,
+                  color: Colors.blue,
+                ),
+                const SizedBox(width: 8.0),
+                Text(
+                  Utils.getStringTimestamp(report.createdAt),
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12.0),
+            Row(
+              children: [
+                const Icon(
+                  Icons.report,
+                  size: 18,
+                  color: Colors.blue,
+                ),
+                const SizedBox(width: 8.0),
+                Text(
+                  report.citizenName,
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ShimmerApprovalReportCard extends StatelessWidget {
+  const ShimmerApprovalReportCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(90),
+            ),
+            child: Text(
+              "Loading",
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          Text(
+            "Loading",
+            style: GoogleFonts.plusJakartaSans(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          Row(
+            children: [
+              const Icon(
+                Icons.calendar_month,
+                size: 18,
+                color: Colors.blue,
+              ),
+              const SizedBox(width: 8.0),
+              Text(
+                "Loading",
+                style: GoogleFonts.plusJakartaSans(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12.0),
+          Row(
+            children: [
+              const Icon(
+                Icons.report,
+                size: 18,
+                color: Colors.blue,
+              ),
+              const SizedBox(width: 8.0),
+              Text(
+                "Loading",
+                style: GoogleFonts.plusJakartaSans(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
